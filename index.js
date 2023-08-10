a0037aa201c85375227b1d8c346a3b59cba39b7f
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2023-08-10 10:56:09 +0530

    created mongodb connection setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,6 +15,23 @@ app.get("/", (req, res) => {
   res.send("Hello! Welcome to blog backend");
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(process.env.PORT || 5000, (err) => {
+      if (err) {
+        return console.log({
+          error: "Can't connect to backend server",
+          message: err,
+        });
+      }
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log({
+      error: "Can't Connect to mongodb",
+      message: err.message,
+    });
+  });
