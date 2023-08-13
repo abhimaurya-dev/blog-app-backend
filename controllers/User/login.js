4931311fec588907be64e2dc4d78323d33ab7243
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2023-08-13 21:53:55 +0530

    added login route and controller

---
+++ controllers/User/login.js
@@ -0,0 +1,26 @@
+import User from "../../model/userModel.js";
+import ErrorHandler from "../../utils/errorHandler.js";
+import bcrypt from "bcryptjs";
+
+export const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const usernameExist = await User.findOne({ username: username });
+    if (!usernameExist) {
+      return next(new ErrorHandler("Invalid Username or Password", 403));
+    }
+    const passwordMatch = await bcrypt.compare(
+      password,
+      usernameExist.password
+    );
+    if (!passwordMatch) {
+      return next(new ErrorHandler("Invalid Username or Password", 403));
+    }
+    res.status(200).json({
+      success: true,
+      message: "Successful login",
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import signUpController from "../controllers/User/signUp.js";
-
+import { login as loginController } from "../controllers/User/login.js";
 const router = express.Router();
 
 router.post("/SignUp", signUpController);
+router.post("/login", loginController);
 
 export default router;
