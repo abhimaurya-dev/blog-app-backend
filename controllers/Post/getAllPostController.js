5fdc6bbd96adc2b0b48ae228443fba5e1c2d875d
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2023-08-19 13:05:16 +0530

    created check in getAllPostController to not to return empty array when no posts found

--- controllers/Post/getAllPostController.js
+++ controllers/Post/getAllPostController.js
@@ -1,8 +1,14 @@
 import Post from "../../model/postModel.js";
+import ErrorHandler from "../../utils/errorHandler.js";
 
 export const getAllPostControllers = async (req, res, next) => {
   try {
-    const allPosts = await Post.find().populate("author").populate("category");
+    const allPosts = await Post.find({})
+      .populate("author")
+      .populate("category");
+    if (!allPosts.length) {
+      return next(new ErrorHandler("Posts not found", 404));
+    }
     res.status(200).json({
       success: true,
       allPosts,
