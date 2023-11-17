f1899988130f3bef087c031d81b76ecf26040744
Abhishek Maurya <100673182+abhimaurya-dev@users.noreply.github.com>
2023-11-17 18:20:11 +0530

    production break debug

--- middlewares/userMiddlewares/isUserLoggedInMiddleware.js
+++ middlewares/userMiddlewares/isUserLoggedInMiddleware.js
@@ -3,8 +3,10 @@ import ErrorHandler from "../../utils/errorHandler.js";
 
 export const isUserLoggedIn = async (req, res, next) => {
   try {
-    const userAuthToken = req.cookies["jwt-token"];
-    console.log(userAuthToken);
+    const userAuthToken = req.headers.authorization.slice(
+      7,
+      req.headers.authorization.length
+    );
     if (!userAuthToken) {
       return next(new ErrorHandler("Unauthorized access", 401));
     }
