2b8828bb68354b062837cb2a96cefdeae4c7db10
Abhishek Maurya <100673182+abhimaurya-dev@users.noreply.github.com>
2023-11-20 16:11:18 +0530

    deployment settings

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Replace with your frontend origin
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   // Add other necessary headers
